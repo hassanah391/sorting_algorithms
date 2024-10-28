01a4afe76518394b62b8afa652f24c392e37636f
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-28 21:19:01 +0300

    Adds prototype of selectionsort to sort.h

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 void swap(int *array, int index1, int index2);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t** head_ref,listint_t* node1, listint_t* node2);
+void selection_sort(int *array, size_t size);
 
 #endif
